fc2dafc55a940210ce1005c3e311d222de535b12
Chris Perrins <chrisperrins95@gmail.com>
2020-04-06 02:09:50 +0100

    Cart test added

--- src/test/java/name/christophperrins/swaglabtesting/Runner.java
+++ src/test/java/name/christophperrins/swaglabtesting/Runner.java
@@ -4,6 +4,8 @@ import org.junit.internal.TextListener;
 import org.junit.runner.JUnitCore;
 
 import name.christophperrins.swaglabtesting.cart.CartTest;
+import name.christophperrins.swaglabtesting.inventory.InventoryTest;
+import name.christophperrins.swaglabtesting.login.LoginTest;
 import name.christophperrins.swaglabtesting.utils.TestUtils;
 
 public class Runner {
@@ -15,7 +17,7 @@ public class Runner {
 		
 		core.addListener(new TextListener(System.out));
 		
-		Class<?>[] classes = {CartTest.class};
+		Class<?>[] classes = {LoginTest.class, InventoryTest.class, CartTest.class};
 		
 		core.run(classes);
 		TestUtils.endReport();
